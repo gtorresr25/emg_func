43141dbab83f4ed4483427fb21aad03787a2064f
gtorresr25 <51379758+gtorresr25@users.noreply.github.com>
2023-02-17 15:23:05 -0500

    Update pxt.json, main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
-basic.forever(function () {
-	
-})
+/**
+ * Use this file to define custom functions and blocks.
+ * 
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+let ave = 0
+let sum = 0
+let list: number[] = []
+let EMG_code = 0
+let winsize = 15
+for (let index = 0; index <= winsize; index++) {
+    EMG_code = pins.analogReadPin(AnalogPin.P3)
+    list.push(EMG_code)
+}
+namespace custom2 {
+    /**
+     * Use dynamic average window to filter EMG_code signal from Pin P3
+     */
+    //% block="EMG_filtered"
+    export function EmgFilter(pin_name: AnalogPin): number {
+        // Add code here
+
+        EMG_code = pins.analogReadPin(pin_name)
+        list.unshift(EMG_code)
+        list.removeAt(winsize)
+        sum = 0
+        for (let index2 = 0; index2 <= winsize; index2++) {
+            sum = sum + list[index2]
+        }
+        ave = sum / winsize
+        return ave
+    }
+}
