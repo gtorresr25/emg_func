b2b953610ded51207138e287f236706160501b66
gtorresr25 <51379758+gtorresr25@users.noreply.github.com>
2023-02-17 15:33:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,27 +7,28 @@ let EMG_code = 0
 let list: number[] = []
 let sum = 0
 let ave = 0
-let winsize = 15
-for (let index = 0; index <= winsize; index++) {
+let window_size = 15
+// Change the EMG name to EMG_code so student's variable will not have conflict
+for (let index = 0; index <= window_size; index++) {
     EMG_code = pins.analogReadPin(AnalogPin.P3)
+    // list.push(0)
     list.push(EMG_code)
 }
-namespace custom2 {
+namespace custom {
     /**
-     * Use dynamic average window to filter EMG_code signal from Pin P3
+     * Use dynamic average window to filter EMG signal from Pin P3
      */
-    //% block="EMG_filtered"
+    //% block="EMG_filtered in the windows size %window_size by pin %pin_name"
     export function EmgFilter(pin_name: AnalogPin): number {
         // Add code here
-
         EMG_code = pins.analogReadPin(pin_name)
         list.unshift(EMG_code)
-        list.removeAt(winsize)
+        list.removeAt(window_size)
         sum = 0
-        for (let index2 = 0; index2 <= winsize; index2++) {
+        for (let index2 = 0; index2 <= window_size; index2++) {
             sum = sum + list[index2]
         }
-        ave = sum / winsize
+        ave = sum / window_size
         return ave
     }
 }
